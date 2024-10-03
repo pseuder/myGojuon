50ae6d4fa94f742790b5b2b7915b6b7110f5eacc
Your Name <you@example.com>
2024-10-03 16:32:22 +0800

    1. 加入作者過濾功能 2. 標題連結可用右鍵打開新標籤

--- src/views/SongOverview.vue
+++ src/views/SongOverview.vue
@@ -1,8 +1,24 @@
 <template>
   <div class="flex flex-col px-10 py-4 gap-4">
+    <div class="flex gap-4 items-center mb-4 select-none">
+      <el-tag
+        class="cursor-pointer text-blue-400 hover:underline hover:text-blue-600"
+        @click="resetFilter"
+      >
+        全部
+      </el-tag>
+      <el-tag
+        v-for="author in allAuthors"
+        :key="author.author"
+        class="cursor-pointer text-blue-400 hover:underline hover:text-blue-600"
+        @click="filterByAuthor(author.author)"
+      >
+        {{ author.author }}
+      </el-tag>
+    </div>
     <div class="flex gap-4 items-center">
       <el-space class="justify-center" style="width: 100%" wrap>
-        <template v-for="video in allVideos" :key="video.UID">
+        <template v-for="video in filteredVideos" :key="video.UID">
           <el-card class="w-full max-w-[380px]" shadow="hover">
             <img
               :src="video.video_thumbnail"
@@ -10,13 +26,12 @@
               alt="video thumbnail"
             />
             <div class="p-4">
-              <el-text
-                class="cursor-pointer text-lg text-blue-400 hover:underline hover:text-blue-600 w-full mb-2"
-                truncated
-                @click="navigateToPractice(video)"
+              <router-link
+                :to="{ name: 'songPractice', params: { id: video.video_id } }"
+                class="text-lg text-blue-400 hover:underline hover:text-blue-600 block w-full mb-2 truncate"
               >
                 {{ video.video_name }}
-              </el-text>
+              </router-link>
             </div>
           </el-card>
         </template>
@@ -26,15 +41,34 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, computed } from "vue";
 import { useRouter } from "vue-router";
 import axios from "@/utils/axios";
 
 const router = useRouter();
 const allVideos = ref([]);
+const allAuthors = ref([]);
+const selectedAuthor = ref(null);
+
+const filteredVideos = computed(() => {
+  if (selectedAuthor.value) {
+    return allVideos.value.filter(
+      (video) => video.author === selectedAuthor.value
+    );
+  }
+  return allVideos.value;
+});
 
 const navigateToPractice = (video) => {
   router.push({ name: "songPractice", params: { id: video.video_id } });
+};
+
+const filterByAuthor = (authorName) => {
+  selectedAuthor.value = authorName;
+};
+
+const resetFilter = () => {
+  selectedAuthor.value = null;
 };
 
 const fetchVideos = async () => {
@@ -42,7 +76,13 @@ const fetchVideos = async () => {
   allVideos.value = data;
 };
 
+const fetchAuthors = async () => {
+  const data = await axios.get("/get_authors");
+  allAuthors.value = data;
+};
+
 onMounted(() => {
   fetchVideos();
+  fetchAuthors();
 });
 </script>
